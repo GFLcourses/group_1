b4c7602a664bcf801ae26f40495e486179365620
Ostryi_Dmitriy_asdasd-mjeesh <201300@nuos.edu.ua>
2022-08-02 17:26:28 +0300

    change codestyle

--- src/main/java/executor/service/model/ThreadPoolConfigDto.java
+++ src/main/java/executor/service/model/ThreadPoolConfigDto.java
@@ -3,22 +3,8 @@ package executor.service.model;
 import java.util.Objects;
 
 public class ThreadPoolConfigDto {
-
     private Integer corePoolSize;
     private Long keepAliveTime;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ThreadPoolConfigDto that = (ThreadPoolConfigDto) o;
-        return Objects.equals(corePoolSize, that.corePoolSize) && Objects.equals(keepAliveTime, that.keepAliveTime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(corePoolSize, keepAliveTime);
-    }
 
     public ThreadPoolConfigDto(Integer corePoolSize, Long keepAliveTime) {
         this.corePoolSize = corePoolSize;
@@ -39,5 +25,26 @@ public class ThreadPoolConfigDto {
 
     public void setKeepAliveTime(Long keepAliveTime) {
         this.keepAliveTime = keepAliveTime;
+    }
+
+    @Override
+    public String toString() {
+        return "ThreadPoolConfigDto{" +
+               "corePoolSize=" + corePoolSize +
+               ", keepAliveTime=" + keepAliveTime +
+               '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ThreadPoolConfigDto that = (ThreadPoolConfigDto) o;
+        return Objects.equals(corePoolSize, that.corePoolSize) && Objects.equals(keepAliveTime, that.keepAliveTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(corePoolSize, keepAliveTime);
     }
 }
