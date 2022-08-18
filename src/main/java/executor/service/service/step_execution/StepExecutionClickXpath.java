104781381614c8cff6ef48259c99e5c948be7d72
Meylin-V <maksymenko.nataliia@gmail.com>
2022-08-18 09:23:34 +0300

    StepExecutionClickXpath class added

--- src/main/java/executor/service/service/step_execution/StepExecutionClickXpath.java
+++ src/main/java/executor/service/service/step_execution/StepExecutionClickXpath.java
@@ -3,14 +3,14 @@ package executor.service.service.step_execution;
 import executor.service.model.Step;
 import org.openqa.selenium.WebDriver;
 
-public class StepExecutionClickXpath implements StepExecutable{
+public class StepExecutionClickXpath implements StepExecutable {
     @Override
     public String getStepAction() {
-        return "clickXPath";
+        return "click";
     }
 
     @Override
     public void step(WebDriver webDriver, Step step) {
-       webDriver.navigate().to(step.getValue());
+        webDriver.navigate().to(step.getValue());
     }
 }
