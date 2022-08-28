137e604663705621602678173e385f83cf3ae4df
Meylin-V <maksymenko.nataliia@gmail.com>
2022-08-28 12:05:01 +0300

    getStepAction() in StepExecutionClickXpath class corrected

--- src/main/java/executor/service/service/step_execution/StepExecutionClickXpath.java
+++ src/main/java/executor/service/service/step_execution/StepExecutionClickXpath.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class StepExecutionClickXpath implements StepExecutable {
     @Override
     public String getStepAction() {
-        return "click";
+        return "clickXpath";
     }
 
     @Override
