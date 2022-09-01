7aab40b77ba2639096deb0eff09b353406458285
AnutaCh <nuta.platina@gmail.com>
2022-09-01 22:52:34 +0300

    Create service Scenario Executor (interface and implement this interface methods)

---
+++ src/main/java/executor/service/service/ScenarioExecutorServiceImpl.java
@@ -0,0 +1,35 @@
+package executor.service.service;
+
+import executor.service.model.Scenario;
+import executor.service.model.Step;
+import executor.service.service.step_execution.StepExecutable;
+import executor.service.service.step_execution.StepExecutionClickCSS;
+import executor.service.service.step_execution.StepExecutionClickXpath;
+import executor.service.service.step_execution.StepExecutionServiceSleep;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+public class ScenarioExecutorServiceImpl implements ScenarioExecutor {
+    @Override
+    public void execute(Scenario scenario, WebDriver webDriver) {
+        List<Step> steps = scenario.getSteps();
+        for (Step step : steps) {
+            String action = step.getAction();
+            StepExecutable stepExecutable = null;
+            switch (action){
+                case "clickCSS":
+                    stepExecutable = new StepExecutionClickCSS();
+                    break;
+                case "clickXpath":
+                    stepExecutable = new StepExecutionClickXpath();
+                    break;
+                case "sleep":
+                    stepExecutable = new StepExecutionServiceSleep();
+                    break;
+                default:
+                    stepExecutable.step(webDriver, step);
+            }
+        }
+    }
+}
