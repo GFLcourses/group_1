f048bfae8318e3efbdfd4c83fb5bb009547ec37f
mirajane2207 <mirajane2207@gmail.com>
2022-09-05 19:07:11 +0300

    Implemented ProxySources client plugin

--- src/main/java/executor/service/service/ProxySourcesClient.java
+++ src/main/java/executor/service/service/ProxySourcesClient.java
@@ -2,6 +2,9 @@ package executor.service.service;
 
 import executor.service.model.ProxyConfigHolder;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public interface ProxySourcesClient {
-    ProxyConfigHolder getProxy();
+    ProxyConfigHolder getProxy() throws URISyntaxException, IOException;
 }
