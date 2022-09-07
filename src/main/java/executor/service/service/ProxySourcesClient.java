7c9ec2ac2544e59cb32c0371ad38a475a5a991e5
Oksana Chernenko <74358660+mirajane2207@users.noreply.github.com>
2022-09-07 20:42:02 +0300

    Revert "Implemented ProxySourcesClient plugin"

--- src/main/java/executor/service/service/ProxySourcesClient.java
+++ src/main/java/executor/service/service/ProxySourcesClient.java
@@ -2,9 +2,6 @@ package executor.service.service;
 
 import executor.service.model.ProxyConfigHolder;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 public interface ProxySourcesClient {
-    ProxyConfigHolder getProxy() throws URISyntaxException, IOException;
+    ProxyConfigHolder getProxy();
 }
