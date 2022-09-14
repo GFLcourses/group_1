96b742a6ac89546fbb559219b8e97c1428027046
Ostryi_Dmitriy_asdasd-mjeesh <201300@nuos.edu.ua>
2022-09-14 14:34:34 +0300

    add also one test method

--- src/test/java/executor/service/service/parallel_stuff/ExecutionServiceImplTest.java
+++ src/test/java/executor/service/service/parallel_stuff/ExecutionServiceImplTest.java
@@ -1,8 +1,9 @@
 package executor.service.service.parallel_stuff;
 
 import executor.service.ScenarioSourceListenerImpl;
-import executor.service.model.Scenario;
 import executor.service.service.ScenarioExecutorServiceImpl;
+import executor.service.service.factory.DIFactory;
+import executor.service.service.factory.Factory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -21,11 +22,20 @@ public class ExecutionServiceImplTest {
     @Mock
     private ScenarioExecutorServiceImpl scenarioExecutor;
     @Mock
-    private ExecutionServiceImpl executionService;
+    private ExecutionServiceImpl mockExecutionService;
 
     @Test
     public void executeTest() {
+        mockExecutionService.execute(webDriver, scenarioSourceListener, scenarioExecutor);
+        verify(mockExecutionService, times(1)).execute(webDriver, scenarioSourceListener, scenarioExecutor);
+    }
+    
+    @Test
+    public void executeTestOfInsideComponents() {
+        Factory factory = DIFactory.getInstance();
+        ExecutionService executionService = factory.getInstance(ExecutionServiceImpl.class);
         executionService.execute(webDriver, scenarioSourceListener, scenarioExecutor);
-        verify(executionService, times(1)).execute(webDriver, scenarioSourceListener, scenarioExecutor);
+        verify(scenarioSourceListener, times(1)).getScenario();
+        verify(scenarioExecutor, times(1)).execute(scenarioSourceListener.getScenario(), webDriver);
     }
 }
