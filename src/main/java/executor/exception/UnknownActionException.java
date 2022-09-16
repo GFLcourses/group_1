4c2e5d9eaa383ab95621f21b3e1018288fb57630
Ostryi_Dmitriy_asdasd-mjeesh <201300@nuos.edu.ua>
2022-09-16 16:07:52 +0300

    exception for ScenarioExecutorServiceImpl

---
+++ src/main/java/executor/exception/UnknownActionException.java
@@ -0,0 +1,8 @@
+package executor.exception;
+
+public class UnknownActionException extends RuntimeException {
+
+    public UnknownActionException(String message) {
+        super(message);
+    }
+}
