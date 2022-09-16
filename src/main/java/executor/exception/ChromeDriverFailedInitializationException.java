1d816ba1121cb64e571dae9e2e043fe31029f090
Ostryi_Dmitriy_asdasd-mjeesh <201300@nuos.edu.ua>
2022-09-16 09:09:00 +0300

    added custom exception for ChromeWebDriverInitializer

---
+++ src/main/java/executor/exception/ChromeDriverFailedInitializationException.java
@@ -0,0 +1,8 @@
+package executor.exception;
+
+public class ChromeDriverFailedInitializationException extends RuntimeException {
+
+    public ChromeDriverFailedInitializationException(Throwable cause) {
+        super(cause);
+    }
+}
